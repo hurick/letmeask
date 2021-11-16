1323b87af96c1db7f25af529c3d44e9b0c706b3e
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2021-11-15 23:24:53 -0300

    feat(Room): create page structure and define route

---
+++ src/pages/Room/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react'
+
+const Room = (): ReactElement => {
+  return (
+    <h1>Sala</h1>
+  )
+}
+
+export default Room
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,14 +5,17 @@ import AuthContextProvider from '../contexts/Auth'
 
 const Home = lazy(() => import('../pages/Home'))
 const CreateRoom = lazy(() => import('../pages/CreateRoom'))
+const Room = lazy(() => import('../pages/Room'))
 
 export const Router = (): ReactElement => (
   <BrowserRouter>
     <Suspense fallback={<div>Loading...</div>}>
       <AuthContextProvider>
         <Routes>
-          <Route path="/" element={<Home/ >} />
-          <Route path="/rooms/create" element={<CreateRoom/ >} />
+          <Route path="/" element={<Home />} />
+
+          <Route path="/rooms/create" element={<CreateRoom />} />
+          <Route path="/rooms/:id" element={<Room />} />
         </Routes>
       </AuthContextProvider>
     </Suspense>
