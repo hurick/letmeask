ab5da1939a57579c0904947d6fb2878a59224edb
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2021-11-21 19:46:38 -0300

    refactor(Question): fixed unnecessary component key prop

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -7,19 +7,19 @@ import likeImg from '../../assets/images/like.svg'
 import styles from './Question.module.sass'
 
 type QuestionProps = {
-  key?: string
   content: string
   authorAvatar: string
   authorName: string
 }
 
-export const Question = ({ key, content, authorAvatar, authorName }: QuestionProps): ReactElement => {
+export const Question = ({ content, authorAvatar, authorName }: QuestionProps): ReactElement => {
   return (
-    <li key={key} className={styles.Question}>
+    <li className={styles.Question}>
       <p className={styles.Q__Content}>{content}</p>
 
       <div className={styles.Q__Footer}>
         <Avatar avatar={authorAvatar} name={authorName} />
+
         <button className={styles.QF__Like}>
           <span className={styles.QFL__Counter}>21</span>
           <img src={likeImg} alt="Thumbs up" />
