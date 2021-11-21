3615eb692e06be6f89d66fe82ff2c2517f67b02f
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2021-11-21 20:59:19 -0300

    feat(Button): added new isOutlined prop to the component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,17 +2,22 @@ import { ReactElement, ButtonHTMLAttributes } from 'react'
 
 import styles from './Button.module.sass'
 
-export const Button = ({ type, className, children, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>): ReactElement => {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean
+}
+
+export const Button = ({ type, className, children, isOutlined = false, ...props }: ButtonProps): ReactElement => {
   const BUTTON_CLASSES = [
     styles.Button,
-    className && className
+    className && className,
+    isOutlined && styles.B__IsOutlined
   ].join(' ')
 
   return (
     <button
       type={type ?? 'button'}
       className={BUTTON_CLASSES}
-      {...rest}
+      {...props}
     >
       {children}
     </button>
