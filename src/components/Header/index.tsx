e8a5ecdcb763ea3f9fc9b36f6f8e83a0fab4528f
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2021-11-21 20:59:53 -0300

    feat(Header): added new isAdmin prop to the component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,21 @@ import { ReactElement } from "react";
 import applicationLogo from '../../assets/images/logo.svg'
 
 import { Code } from '../Code'
+import { Button } from "../Button"
 
 import styles from './Header.module.sass'
 
 type HeaderProps = {
   roomId: string
+  isAdmin?: boolean
 }
 
-export const Header = ({ roomId }: HeaderProps): ReactElement => {
+export const Header = ({ roomId, isAdmin }: HeaderProps): ReactElement => {
+  const ACTIONS_CLASES = [
+    styles.H__Actions,
+    isAdmin && styles.HA__Multiple
+  ].join(' ')
+
   return (
     <header className={styles.Header}>
       <img
@@ -19,8 +26,9 @@ export const Header = ({ roomId }: HeaderProps): ReactElement => {
         className={styles.H__Logo}
       />
 
-      <div className={styles.H__Room}>
+      <div className={ACTIONS_CLASES}>
         <Code code={roomId} />
+        { isAdmin && <Button isOutlined className={styles.HAM__Close}>Encerrar sala</Button> }
       </div>
     </header>
   )
