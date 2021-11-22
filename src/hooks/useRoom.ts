bb1ef5f857bb78710cfce7dae31376542ce1efec
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2021-11-22 00:53:19 -0300

    feat(useRoom): added question like features and infos

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,6 +1,18 @@
 import { useState, useEffect } from 'react'
 
-import { getDatabase, ref, onValue } from 'firebase/database'
+import { getDatabase, ref, onValue, off } from 'firebase/database'
+import { useAuth } from './useAuth'
+
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string
+    avatar: string
+  }
+  content: string
+  isAnswered: boolean
+  isHighlighted: boolean
+  likes: Record<string, { authorId: string }>
+}>
 
 type QuestionListTypes = {
   id: string
@@ -11,19 +23,13 @@ type QuestionListTypes = {
   content: string
   isAnswered: boolean
   isHighlighted: boolean
+  likeCount: number
+  likeId: string | undefined
 }
 
-type FirebaseQuestions = Record<string, {
-  author: {
-    name: string
-    avatar: string
-  }
-  content: string
-  isAnswered: boolean
-  isHighlighted: boolean
-}>
-
 export const useRoom = (params: { id: string }) => {
+  const { user } = useAuth()
+
   const [roomTitle, setRoomTitle] = useState<string>('')
   const [questionsList, setQuestionsList] = useState<QuestionListTypes[]>([])
 
@@ -38,13 +44,17 @@ export const useRoom = (params: { id: string }) => {
         content: value.content,
         author: value.author,
         isHighlighted: value.isHighlighted,
-        isAnswered: value.isAnswered
+        isAnswered: value.isAnswered,
+        likeCount: Object.values(value.likes ?? {}).length,
+        likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
       }))
 
       setRoomTitle(databaseRoom.title)
       setQuestionsList(parsedQuestions)
     })
-  }, [params.id])
+
+    return () => off(roomRef)
+  }, [params.id, user?.id])
 
   return { roomTitle, questionsList }
 }
