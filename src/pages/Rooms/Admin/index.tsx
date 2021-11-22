04bb25d2f7a43dcc791ef8ad2da26ed1fd00eefd
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2021-11-22 01:18:57 -0300

    feat(Rooms/Admin): created delete question and end room methods

--- src/pages/Rooms/Admin/index.tsx
+++ src/pages/Rooms/Admin/index.tsx
@@ -1,21 +1,43 @@
 import { Fragment, ReactElement } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 
-import { useParams } from 'react-router-dom'
+import { ref, getDatabase, remove, update } from 'firebase/database'
+
 import { useRoom } from '../../../hooks/useRoom'
 
 import { Header } from '../../../components/Header'
 import { Question } from '../../../components/Question'
 
+import deleteImg from '../../../assets/images/delete.svg'
+
 import styles from './Admin.module.sass'
 
 const Admin = (): ReactElement => {
+  const navigate = useNavigate()
   const params = useParams() as { id: string }
 
   const { roomTitle, questionsList } = useRoom(params)
 
+  const handleDelete = async (questionId: string): Promise<void> => {
+    window.confirm('Are you sure you want to delete this question?')
+      && await remove(ref(getDatabase(), `/rooms/${params.id}/questions/${questionId}`))
+  }
+
+  const handleEndRoom = async (): Promise<void> => {
+    await update(ref(getDatabase(), `/rooms/${params.id}`), {
+      closedAt: new Date()
+    })
+
+    navigate('/')
+  }
+
   return (
     <Fragment>
-      <Header isAdmin roomId={params.id} />
+      <Header
+        roomId={params.id}
+        isAdmin
+        onEndRoom={handleEndRoom}
+      />
 
       <main className={styles.R__Main}>
         <h1 className={styles.RM__Heading}>
@@ -33,7 +55,15 @@ const Admin = (): ReactElement => {
                 content={question.content}
                 authorAvatar={question.author.avatar}
                 authorName={question.author.name}
-              />
+              >
+                <button
+                  className={styles.RMQ__Delete}
+                  title="Delete this question"
+                  onClick={() => handleDelete(question.id)}
+                >
+                  <img src={deleteImg} alt="Trash can" />
+                </button>
+              </Question>
             ))
             : <p>No questions yet. Be the first one!</p>
           }
